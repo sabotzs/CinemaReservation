5e65a70c7f35b48f3c4ad9efccaca75326b9bcd3
Zlatina Cholakova <zlatinatc99@gmail.com>
2020-05-13 16:51:20 +0300

    add 2 queries in projection queries

--- projections/queries.py
+++ projections/queries.py
@@ -19,3 +19,38 @@ DELETE_PROJECTION = '''
     DELETE FROM projections
         WHERE id = ?;
 '''
+
+SELECT_PROJECTIONS = '''
+    SELECT movies.name, projections.id, day, hour, movie_type, COUNT(reservations.id) AS reserv_count
+        FROM projections
+        LEFT JOIN reservations
+            ON projections.id = reservations.projection_id
+        JOIN movies
+            ON projections.movie_id = movies.id
+        WHERE movie_id = ?
+        GROUP BY projections.id;
+'''
+
+SLECT_TAKEN_SEATS = '''
+    SELECT name, rating, day, hour, movie_type
+        FROM projections
+        JOIN movies
+            ON movies.id = projections.movie_id
+        WHERE projections.id = ?;
+'''
+
+GET_ALL_PROJECTIONS = '''
+    SELECT name, projections.id AS id, day, hour, movie_type, COUNT(reservations.id) AS reserv_count
+        FROM projections
+        JOIN movies
+            ON projections.movie_id = movies.id
+        LEFT JOIN reservations
+            ON projections.id = reservations.projection_id
+        GROUP BY projections.id;
+'''
+
+SLECT_MOVIES = '''
+    SELECT id, name, rating
+        FROM movies
+        ORDER BY rating;
+'''
